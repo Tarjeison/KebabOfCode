26942ac580c0da48ec07e5485abb9ee69bb95ed2
CO\ea.gjolstad <ea.gjolstad@creuna.no>
2020-12-02 10:20:47 +0100

    Endre advent of code initial commit

---
+++ endre/AdventOfCode/AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day1
+{
+    [TestFixture]
+    class Day1
+    {
+        public void Get2020ByTwoBinomialsFromList()
+        {
+            var wantedAnswer = 2020;
+            string[] input = System.IO.File.ReadAllLines(@"C:\\src\\local\\CodeWars\\Kata\\CodeWars\\Kata1\\AdventOfCode\\Day1\\input.txt");
+            var int_input = input.Select(x => int.Parse(x)).ToList();
+            var resultList = new List<int>();
+
+            for (int i = 0; i < int_input.Count; i++)
+            {
+                var currentValue = int_input[i];
+                for (int j = i + 1; j < int_input.Count; j++)
+                {
+                    for (int k = j + 1; k < int_input.Count; k++)
+                    {
+                        var comparatorValueOne = int_input[j];
+                        var comparatorValueTwo = int_input[k];
+                        if (currentValue + comparatorValueOne + comparatorValueTwo == wantedAnswer)
+                        {
+                            resultList.Add(comparatorValueOne);
+                            resultList.Add(comparatorValueTwo);
+                            resultList.Add(currentValue);
+                        }
+                    }
+                }
+            }
+
+            var result = resultList[0] * resultList[1] * resultList[2];
+        }
+    }
+}
---
+++ endre/AdventOfCode/AdventOfCode/Day2/Day2.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day2
+{
+    class Day2
+    {
+        [Test]
+        public void CheckPasswordIntegrityFromInputFile()
+        {
+            string[] rawInput = System.IO.File.ReadAllLines(@"C:\\src\\local\\CodeWars\\Kata\\CodeWars\\Kata1\\AdventOfCode\\Day2\\input.txt");
+            var input = rawInput.Select(x => x).ToList();
+            int result = 0;
+            foreach (string s in input)
+            {
+                if (IsThisPasswordAccepted(s))
+                    result++;
+            }
+
+            var debugpoint = 0;
+        }
+
+        public bool IsThisPasswordAccepted(string line)
+        {
+            var lineSplit = line.Split(":");
+            var policy = lineSplit[0];
+            var password = lineSplit[1].Replace(" ", "");
+            var letter = policy.Split(" ")[1].ToCharArray();
+            var lowConstraint = int.Parse(policy.Split(" ")[0].Split("-")[0]);
+            var highConstraint = int.Parse(policy.Split(" ")[0].Split("-")[1]);
+
+
+            var positionOfPolicyLetter = password.IndexOf(letter.First()) + 1;
+            var listOfPositions = new List<int>();
+            while (positionOfPolicyLetter != 0)
+            {
+                listOfPositions.Add(positionOfPolicyLetter);
+                positionOfPolicyLetter = password.IndexOf(letter.First(), positionOfPolicyLetter) + 1;
+            }
+
+            if (listOfPositions.Any(x => x == lowConstraint) && listOfPositions.Any(x => x == highConstraint))
+                return false;
+            else if (listOfPositions.Any(x => x == lowConstraint))
+                return true;
+            else if (listOfPositions.Any(x => x == highConstraint))
+                return true;
+
+            return false;
+        }
+    }
+}
